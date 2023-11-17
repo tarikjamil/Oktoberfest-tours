0a119712292ba6413907ce23d01f55b449239e19
tarikjamil <tarikjamill@gmail.com>
2023-11-17 22:01:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -51,16 +51,38 @@ document.addEventListener("DOMContentLoaded", function () {
   sliders.forEach(function (slider) {
     let splide = new Splide(slider, {
       type: "slide",
-      perPage: 1,
+      perPage: 2,
       perMove: 1,
       gap: "24rem",
-      drag: false,
       arrows: false,
       pagination: false,
       breakpoints: {
         991: {
-          // Tablet
-          gap: "24rem",
+          perPage: 1,
+          drag: true,
+        },
+      },
+    });
+    splide.mount();
+  });
+});
+
+// slider resources
+document.addEventListener("DOMContentLoaded", function () {
+  // Select all .is--resources-slider elements
+  let sliders = document.querySelectorAll(".is--testimonial-slider");
+
+  // For each slider, initialize Splide
+  sliders.forEach(function (slider) {
+    let splide = new Splide(slider, {
+      type: "slide",
+      perPage: 1,
+      perMove: 1,
+      gap: "24rem",
+      arrows: false,
+      pagination: false,
+      breakpoints: {
+        991: {
           drag: true,
         },
       },
